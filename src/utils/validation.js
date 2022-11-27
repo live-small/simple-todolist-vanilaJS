063672c7c672066869feea3b889afea49383e4fd
suhwa <hansuhwa2005@gmail.com>
2022-11-27 21:05:20 +0900

    typeof -> type check 함수

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,14 +1,17 @@
-const isValidTodoList = todoList => {
+const isValidType = (value, type) => {
+    return typeof value === type;
+};
+
+const isValidTodoList = (todoList) => {
     if (!checkTypeOfTodoList(todoList)) return false;
 
     const requiredProperty = ["text", "isCompleted"];
-    const isValid = todoList.every(todo => {
-        const validProperty = requiredProperty.every(property => property in todo);
+    const isValid = todoList.every((todo) => {
+        const validProperty = requiredProperty.every((property) => property in todo);
 
         if (validProperty) {
-            const isValidType =
-                typeof todo.text === "string" && typeof todo.isCompleted === "boolean";
-            return isValidType;
+            const { text, isCompleted } = todo;
+            return isValidType(text, "string") && isValidType(isCompleted, "boolean");
         }
         return false;
     });
@@ -16,9 +19,9 @@ const isValidTodoList = todoList => {
     return isValid;
 };
 
-const checkTypeOfTodoList = todoList => {
+const checkTypeOfTodoList = (todoList) => {
     if (!Array.isArray(todoList)) return false;
-    const isValidTodoItemType = todoList.every(todo => typeof todo === "object");
+    const isValidTodoItemType = todoList.every((todo) => isValidType(todo, "object"));
 
     return isValidTodoItemType;
 };
